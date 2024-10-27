9cd6c2075a7be87911abe7e344852ef9de25194b
Ali Taghawi <marshaltaghawi@gmail.com>
2024-10-27 14:05:33 +0330

    <fake store app> config axios in main.jsx

--- tutorial/projects/fake-store-app/src/main.jsx
+++ tutorial/projects/fake-store-app/src/main.jsx
@@ -2,6 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import axios from 'axios'
+
+axios.defaults.baseURL = "https://fakestoreapi.com"
+axios.interceptors.response.use(res => res.data, err => console.log(err.message))
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
